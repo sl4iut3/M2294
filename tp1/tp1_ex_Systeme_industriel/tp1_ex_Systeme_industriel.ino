d46c2575cf65b7a0a4c1839bf64d668ae1b982bf
root <root@debian450>
2013-11-03 21:40:37 +0100

    rajout doc

---
+++ tp1/tp1_ex_Systeme_industriel/tp1_ex_Systeme_industriel.ino
@@ -0,0 +1,38 @@
+
+
+#define ledRouge 6
+#define ledVerte 7
+
+#define bp0 2
+#define bp1 3
+#define bp2 A0
+
+char etatPasse_bp0=0, etatPasse_bp2=0, etatPresent=0, etatSortie=0;
+
+void setup() {
+  pinMode(bp0,INPUT);
+  pinMode(bp1,INPUT);
+  pinMode(bp2,INPUT);
+  pinMode(ledRouge,OUTPUT);
+  pinMode(ledVerte,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // teste appui sur bp0 (marche)
+  etatPresent=digitalRead(bp0);
+  if ( (etatPasse_bp0==LOW)&&(etatPresent==HIGH) ) 
+    digitalWrite(ledVerte,HIGH);
+  etatPasse_bp0=etatPresent;
+  // teste appui bp2
+  etatPresent=digitalRead(bp2);
+  if ( (etatPasse_bp2==HIGH)&&(etatPresent==LOW) ) 
+    digitalWrite(ledVerte,LOW);
+  etatPasse_bp2=etatPresent;
+  // teste si bp2==low
+  if (etatPresent==LOW) 
+    digitalWrite(ledRouge,HIGH);
+    else
+    digitalWrite(ledRouge,LOW);
+  delay(50);
+}
